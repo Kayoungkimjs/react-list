bf8cc3a2d692e426a1cea45008271131a315a534
kim_kayoung <kim_kayoung@cocone.co.jp>
2024-02-21 14:55:55 +0900

    💄 Add global styles

---
+++ src/globalStyles.ts
@@ -0,0 +1,59 @@
+import { css } from '@emotion/react'
+import emotionNormalize from 'emotion-normalize'
+import { visuallyHidden } from './utils/a11y'
+
+export const GlobalStyles = css`
+  ${emotionNormalize}
+
+  * {
+    font-family: 'Noto Sans KR', 'Noto Sans', sans-serif;
+    margin: 0;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-family: 'Noto Sans KR', 'Noto Sans', sans-serif;
+  }
+
+  body {
+    color: #757575;
+    background-color: #aaaaaa;
+  }
+
+  a,
+  button {
+    cursor: pointer;
+  }
+
+  button {
+    background: transparent;
+    border: 0;
+    padding: 0;
+
+    &:focus {
+      outline: none;
+      box-shadow: none;
+    }
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  ul,
+  ol {
+    padding-left: 0;
+    list-style: none;
+  }
+
+  h1 {
+    margin: 0;
+  }
+
+  .visuallyHidden {
+    ${visuallyHidden()}
+  }
+`
