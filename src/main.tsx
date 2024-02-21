31ce5a70d12671dad8e2d3d9a29cbb705d4c2e1f
kim_kayoung <kim_kayoung@cocone.co.jp>
2024-02-21 14:45:36 +0900

    🔧 Initial settings for project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { List } from './components'
+import { Global } from '@emotion/react'
+import { GlobalStyles } from './globalStyles.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Global styles={GlobalStyles} />
+    <List />
+  </React.StrictMode>
 )
