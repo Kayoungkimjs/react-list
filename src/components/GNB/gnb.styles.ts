6396db161ad42423e9de77c5fa39239091f9001c
Kim Kayoung <kykim.dev@gmail.com>
2024-03-10 21:50:00 +0900

    💄 Change GNB menu background color

--- src/components/GNB/gnb.styles.ts
+++ src/components/GNB/gnb.styles.ts
@@ -3,6 +3,6 @@ import styled from '@emotion/styled'
 export const GnbContainer = styled.div`
   width: 100%;
   height: 48px;
-  background-color: blue;
+  background-color: #fdded8;
   margin-bottom: 100px;
 `
